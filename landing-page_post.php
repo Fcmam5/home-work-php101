b083e3b664fd21fce3aabed785019503cb07f67c
fcmam5 <dr.fcmam5@gmail.com>
2016-05-01 00:36:13 +0100

    :construction: done using POST methode

---
+++ landing-page_post.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing page (using POST)</title>
+</head>
+<body>
+<?php echo "Hello ".$_POST["urname"]; ?>
+</body>
+</html>
---
+++ name-asking_post.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Asking for your name</title>
+</head>
+<body>
+<form method="POST" action="landing-page_post.php ">
+	<input placeholder="Your name" name="urname" required>
+	<button type="submit">Submit !</button>
+	</form>
+</body>
+</html>
